557c571d008727ae9bc51799ef860e672182d131
SeoDonggwon <ehdrnjsdl920@naver.com>
2022-11-23 22:54:56 +0900

    회원 관리 예제를 위한 회원 Repository 생성

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,43 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    // stream으로 무한루프를 돌리고 filter 안에 람다식을 이용하여
+    // member의 이름이 메소드의 파라미터로 전달받은 이름과 같은지 비교하고
+    // 루프를 돌다가 있으면 바로 리턴, 없으면 Optional에 null이 붙어서 리턴
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals((name)))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
